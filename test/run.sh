61b6d3f2b27d9e420d596272812f5038ef975b52
ArKniiD <n.hallouin@gmail.com>
2017-07-09 18:39:41 +0200

    Updated to 2.6.0 version

--- test/run.sh
+++ test/run.sh
@@ -18,7 +18,7 @@ echo "*                                              *"
 echo "*    generate a robot from noraui-archetype    *"
 echo "*                                              *"
 echo "************************************************"
-mvn archetype:generate -DarchetypeGroupId=com.github.noraui -DarchetypeArtifactId=noraui-archetype -DarchetypeVersion=2.5.3 -DgroupId=com.your.company -DartifactId=robot -DinteractiveMode=false
+mvn archetype:generate -DarchetypeGroupId=com.github.noraui -DarchetypeArtifactId=noraui-archetype -DarchetypeVersion=2.6.0-DgroupId=com.your.company -DartifactId=robot -DinteractiveMode=false
 
 pwd
 ls -l
