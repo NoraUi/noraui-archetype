beecf6a7bf2ea8b9a89b664f6cbff376b76c0bbb
Stéphane GRILLON <sgrillon14@users.noreply.github.com>
2017-10-05 12:36:06 +0200

    add version=0.0.1-SNAPSHOT (robot generated)

--- test/run.sh
+++ test/run.sh
@@ -18,7 +18,7 @@ echo "*                                              *"
 echo "*    generate a robot from noraui-archetype    *"
 echo "*                                              *"
 echo "************************************************"
-mvn archetype:generate -DarchetypeGroupId=com.github.noraui -DarchetypeArtifactId=noraui-archetype -DarchetypeVersion=2.6.2 -DgroupId=com.your.company -DartifactId=robot -DinteractiveMode=false
+mvn archetype:generate -DarchetypeGroupId=com.github.noraui -DarchetypeArtifactId=noraui-archetype -DarchetypeVersion=2.6.2 -DgroupId=com.your.company -DartifactId=robot -Dversion=0.0.1-SNAPSHOT -DinteractiveMode=false
 
 pwd
 ls -l
