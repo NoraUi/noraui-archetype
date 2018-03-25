b4a4bf8549dcc49da7529579f3eeec6ed69f32bb
sgrillon14 <sgrillon@hotmail.fr>
2018-03-25 14:04:38 +0200

    add CLI

--- src/main/resources/archetype-resources/src/main/java/cli/NoarUiCLI.java
+++ src/main/resources/archetype-resources/src/main/java/cli/NoarUiCLI.java
@@ -9,95 +9,23 @@
  */
 package ${package}.cli;
 
-import com.github.noraui.cli.Application;
-import com.github.noraui.cli.Scenario;
-import com.your.company.utils.NoraRobotContext;
-
+import com.github.noraui.cli.NoraUiCommandLineInterface;
 import ${package}.utils.${robotName}Context;
 
 public class NoarUiCLI {
-
+    
     /**
      * Java sample:
      * > cd target\classes
-     * > java -cp . ${package}.NoarUiCLI -h
-     * 
+     * > java -cp . ${package}.cli.NoarUiCLI -h
      * Maven sample:
      * > mvn exec:java -Dexec.mainClass="${package}.cli.NoarUiCLI" -Dexec.args="-h"
      * 
      * @param args
-     *            is list of args (-h, --verbose, -f, -s, -u, -d and -a)
+     *            is list of args (-h, --verbose, --interactiveMode, -f, -s, -u, -d, -a, -m, -fi and -re (optional))
      */
     public static void main(String[] args) {
-        boolean verbose = false;
-        String feature = null;
-        String applicationName = null;
-        String scenarioName = null;
-        String url = null;
-        String description = null;
-
-        System.out.println("");
-        System.out.println("  ███╗   ██╗ ██████╗ ██████╗  █████╗ ██╗   ██╗██╗      ██████╗██╗     ██╗ ");
-        System.out.println("  ████╗  ██║██╔═══██╗██╔══██╗██╔══██╗██║   ██║██║     ██╔════╝██║     ██║ ");
-        System.out.println("  ██╔██╗ ██║██║   ██║██████╔╝███████║██║   ██║██║     ██║     ██║     ██║ ");
-        System.out.println("  ██║╚██╗██║██║   ██║██╔══██╗██╔══██║██║   ██║██║     ██║     ██║     ██║ ");
-        System.out.println("  ██║ ╚████║╚██████╔╝██║  ██║██║  ██║╚██████╔╝██║     ╚██████╗███████╗██║ ");
-        System.out.println("  ╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚═╝      ╚═════╝╚══════╝╚═╝ ");
-        System.out.println("");
-        System.out.println(" NoraUi Command Line Interface =>");
-        System.out.println("");
-
-        if (args.length == 0 || args.length == 1 && args[0].equals("-h")) {
-            System.out.println("-h: Display this help");
-            System.out.println("--verbose: Add debug informations in console.");
-            System.out.println("-f: features 1 => add new application");
-            System.out.println("             2 => add new scenario");
-            System.out.println("             3 => remove old scenario");
-            System.out.println("             4 => remove application");
-            System.out.println("-s: Scenario Name");
-            System.out.println("-u: Url");
-            System.out.println("-d: Description");
-            System.out.println("-a: Application Name");
-        }
-
-        for (int i = 0; i < args.length; i++) {
-            if ("--verbose".equals(args[i])) {
-                verbose = true;
-            } else if ("-f".equals(args[i])) {
-                feature = args[i + 1];
-            } else if ("-s".equals(args[i])) {
-                scenarioName = args[i + 1];
-            } else if ("-u".equals(args[i])) {
-                url = args[i + 1];
-            } else if ("-d".equals(args[i])) {
-                description = args[i + 1];
-            } else if ("-a".equals(args[i])) {
-                applicationName = args[i + 1];
-            }
-        }
-
-        if (verbose) {
-            System.out.print("Command Line: ");
-            for (String arg : args) {
-                System.out.print(" " + arg);
-            }
-            System.out.println("");
-        }
-        
-        if ("1".equals(feature)) {
-            Application application = new Application();
-            application.add(applicationName, url, ${robotName}Context.class, "${robotName}", verbose);
-        } else if ("2".equals(feature)) {
-            Scenario scenario = new Scenario();
-            scenario.add(scenarioName, description, applicationName, "${robotName}", verbose);
-        } else if ("3".equals(feature)) {
-            Scenario scenario = new Scenario();
-            scenario.remove(scenarioName, description, applicationName, "${robotName}", verbose);
-        } else if ("4".equals(feature)) {
-            Application application = new Application();
-            application.remove(applicationName, url, ${robotName}Context.class, "${robotName}", verbose);    
-        }
-
+        new NoraUiCommandLineInterface().runCli(${robotName}Context.class, args);
     }
 
 }
