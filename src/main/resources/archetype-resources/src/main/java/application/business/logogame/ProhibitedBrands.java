b017ce252ebd2642ac2a72ff5c17493d74ba690c
Stéphane GRILLON <sgrillon@hotmail.fr>
2019-08-27 11:24:58 +0200

    ctrl+L

--- src/main/resources/archetype-resources/src/main/java/application/business/logogame/ProhibitedBrands.java
+++ src/main/resources/archetype-resources/src/main/java/application/business/logogame/ProhibitedBrands.java
@@ -1,6 +1,4 @@
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
+#set($symbol_pound='#')#set($symbol_dollar='$')#set($symbol_escape='\')
 /**
  * ${robotName} generated free by NoraUi Organization https://github.com/NoraUi
  * ${robotName} is licensed under the license BSD.
@@ -17,7 +15,7 @@ import java.util.List;
 
 public class ProhibitedBrands {
 
- // Tabaco
+    // Tabaco
     private static final String MARLBORO = "Marlboro";
     private static final String CAMEL = "Camel";
     private static final String PHILIPPE_MORRIS = "philippe morris";
@@ -26,9 +24,9 @@ public class ProhibitedBrands {
     private static final String HEINEKEN = "heineken";
     private static final String ZUBROWKA = "zubrowka";
     private static final String JACK_DANIELS = "jack daniels";
-    
-    private List<String> tabaco = new ArrayList<>(Arrays.asList(MARLBORO,CAMEL,PHILIPPE_MORRIS));
-    private List<String> alcool = new ArrayList<>(Arrays.asList(HEINEKEN,ZUBROWKA,JACK_DANIELS));
+
+    private List<String> tabaco = new ArrayList<>(Arrays.asList(MARLBORO, CAMEL, PHILIPPE_MORRIS));
+    private List<String> alcool = new ArrayList<>(Arrays.asList(HEINEKEN, ZUBROWKA, JACK_DANIELS));
 
     public List<String> getTabaco() {
         return tabaco;
