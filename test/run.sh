892237635e6d14366bbfe0329387a3feea31b467
Stéphane GRILLON <sgrillon@hotmail.fr>
2019-08-31 08:51:30 +0200

    use new format version in CI/CD

--- test/run.sh
+++ test/run.sh
@@ -18,7 +18,7 @@ echo "*                                              *"
 echo "*    generate a robot from noraui-archetype    *"
 echo "*                                              *"
 echo "************************************************"
-mvn archetype:generate -B -DarchetypeGroupId=com.github.noraui -DarchetypeArtifactId=noraui-archetype -DarchetypeVersion=4.0.0-SNAPSHOT -DgroupId=com.your.company -DartifactId=robot -Dversion=0.0.1-SNAPSHOT -DinteractiveMode=false
+mvn archetype:generate -B -DarchetypeGroupId=com.github.noraui -DarchetypeArtifactId=noraui-archetype -DarchetypeVersion=4.0.0.0-SNAPSHOT -DgroupId=com.your.company -DartifactId=robot -Dversion=0.0.1-SNAPSHOT -DinteractiveMode=false
 
 pwd
 ls -l
