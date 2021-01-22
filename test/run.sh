9799e4ae0745e827f9fff987a89374e5199d69ff
Stéphane GRILLON <sgrillon@hotmail.fr>
2021-01-22 11:53:54 +0100

    Migration https://travis-ci.org to https://travis-ci.com

--- test/run.sh
+++ test/run.sh
@@ -36,7 +36,7 @@ echo "*                                                        *"
 echo "**********************************************************"
 mvn clean test javadoc:javadoc -B -Dcucumber.options="--tags '@loginLogout or @playToLogoGame or @jouerAuJeuDesLogos or @search'" -Pjavadoc,drivers,preIC,scenarioInitiator,ci,unit-tests,postIC --settings ../../test/mvnsettings.xml -Dmaven.test.failure.ignore=true -Dcrypto.key=${CRYPTO_KEY}
 
-curl -s "https://api.travis-ci.org/jobs/${TRAVIS_JOB_ID}/log.txt?deansi=true" > nonaui.log
+curl -s "https://api.travis-ci.com/jobs/${TRAVIS_JOB_ID}/log.txt?deansi=true" > nonaui.log
 sleep 15
 
 echo "Log url is https://api.travis-ci.org/jobs/${TRAVIS_JOB_ID}/log.txt?deansi=true"
