7e052676f3eee1f9b7d840126e2b484916c1a5f5
Stéphane GRILLON <sgrillon@hotmail.fr>
2021-01-27 11:00:39 +0100

    migration to Travis API V3

--- test/run.sh
+++ test/run.sh
@@ -36,8 +36,10 @@ echo "*                                                        *"
 echo "**********************************************************"
 mvn clean test javadoc:javadoc -B -Dcucumber.options="--tags '@loginLogout or @playToLogoGame or @jouerAuJeuDesLogos or @search'" -Pjavadoc,drivers,preIC,scenarioInitiator,ci,unit-tests,postIC --settings ../../test/mvnsettings.xml -Dmaven.test.failure.ignore=true -Dcrypto.key=${CRYPTO_KEY}
 
-echo "Log url is https://api.travis-ci.com/v3/job/${TRAVIS_JOB_ID}/log.txt?deansi=true"
-curl -s "https://api.travis-ci.com/jobs/${TRAVIS_JOB_ID}/log.txt?deansi=true" > nonaui.log
+#
+# read Maven console
+echo "******** Travis logs avalable here: https://api.travis-ci.com/v3/job/${TRAVIS_JOB_ID}/log.txt?deansi=true"
+curl -s "https://api.travis-ci.com/v3/job/${TRAVIS_JOB_ID}/log.txt?deansi=true" > nonaui.log
 sleep 15
 
 echo "***************************************************"
